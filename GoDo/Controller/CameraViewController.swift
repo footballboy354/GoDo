a6b03a00d4dfa8521b86aa40db02b4ed2533a088
lit-syunsuke <syunsuke_11@icloud.com>
2025-02-23 15:53:52 +0900

    追加パッケージの追加

--- GoDo/Controller/CameraViewController.swift
+++ GoDo/Controller/CameraViewController.swift
@@ -7,31 +7,59 @@
 
 import UIKit
 
+import PKHUD
+
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     @IBOutlet var cameraImageView: UIImageView!
-
+    
+    // カメラが開かれたかどうかを判定する変数
+    private var isCameraOpened = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
+    //　画面が表示されようとする時
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-//        if UIImagePickerController.isSourceTypeAvailable(.camera){
-//            
-//            let picker = UIImagePickerController()
-//            picker.sourceType = .camera
-//            picker.delegate = self
-//            
-//            picker.allowsEditing = true
-//            
-//            present(picker, animated: true, completion: nil)
-//        } else {
-//            
-//            print("error")
-//        }
+        // カメラがまだ開かれていない時
+        if !isCameraOpened {
+            isCameraOpened = true
+            openCamera()
+        }
     }
-
+    
+    // 📸 カメラを開く処理
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.allowsEditing = true
+            present(picker, animated: true, completion: nil)
+        } else {
+            print("カメラが利用できません")
+        }
+    }
+    
+    // ❌ キャンセル時に1つ目のタブへ戻る
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true) {
+            self.isCameraOpened = false // 🔹 フラグをリセットして再度カメラが開くのを防ぐ
+            self.tabBarController?.selectedIndex = 0 // 🟢 1つ目のタブに移動
+            
+        }
+    }
+    
+    // 🖼️ 画像選択時の処理（必要なら実装）
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true) {
+            
+            self.cameraImageView.image = info[.editedImage] as? UIImage
+            self.isCameraOpened = false // 🔹 フラグをリセットして再度カメラが開くのを防ぐ
+        }
+    }
 }
